a3057e88fbc3038ec77cb25e1a6357a98924cd02
ROSA <gojiradam@gmail.com>
2016-06-26 17:46:06 +0300

    Init Game

--- src/Game.java
+++ src/Game.java
@@ -7,8 +7,12 @@ public class Game extends Canvas implements Runnable {
     public static final int SCALE = 2;
     public final String TITLE = "2D Game";
 
-    public static void main(String args[]){
-        
+    public static void main(String args[]) {
+        Game game = new Game();
+        game.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        game.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        game.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+
     }
 
     @Override
