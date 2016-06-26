33a684ec1623a4016c5cae7adbacbd288b2d0532
ROSA <gojiradam@gmail.com>
2016-06-26 17:44:01 +0300

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable {
+
+    public static final int WIDTH = 20;
+    public static final int HEIGHT = WIDTH / 19 * 9;
+    public static final int SCALE = 2;
+    public final String TITLE = "2D Game";
+
+    public static void main(String args[]){
+        
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
