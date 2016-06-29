2d702d349132ef2678b5417e2640f3a814c192dc
ROSA <gojiradam@gmail.com>
2016-06-29 14:03:40 +0300

    Init Game

---
+++ src/EntityA.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public interface EntityA {
+
+    public void tick();
+    public void render(Graphics g);
+
+    public double getX();
+    public double getY();
+
+}
---
+++ src/EntityB.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public interface EntityB {
+
+    public void tick();
+    public void render(Graphics g);
+
+    public double getX();
+    public double getY();
+
+}
